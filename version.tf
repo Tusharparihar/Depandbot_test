da97578796a1c36c5efd839f5ae837bb902a8364
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-09-09 12:52:40 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -5,25 +5,25 @@ terraform {
     # 1️⃣ Pinned version - Dependabot will bump
     pinned_provider = {
       source  = "hashicorp/null"
-      version = "3.0.0"
+      version = "2.0.0"
     }
 
     # 2️⃣ Single minimum - Dependabot will bump because we pick a low version
     min_provider = {
       source  = "hashicorp/random"
-      version = ">= 3.0.0"
+      version = ">= 1.0.0"
     }
 
     # 3️⃣ Range version - Dependabot will bump if latest outside range
     range_provider = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.11.0, <= 4.19.0"
+      version = ">= 4.11.0, <= 4.13.0"
     }
 
     # 4️⃣ Pessimistic version (~>) - Dependabot will bump if latest outside range
     pessimistic_provider = {
       source  = "aztfmod/azurecaf"
-      version = "~> 1.2.0"
+      version = "~> 1.1.0"
     }
   }
 }
