4247f53924935fec7ca465a5b69b131460a4f292
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-09-10 14:56:30 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -4,37 +4,37 @@ terraform {
   required_providers {
     null = {
       source  = "hashicorp/null"
-      version = "3.2.4"  # latest patch: 3.2.4 → minor/patch PR
+      version = "3.1.2"  # latest patch: 3.2.4 → minor/patch PR
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "3.7.2"  # latest minor: 3.2.0 → minor/patch PR
+      version = "2.0.0"  # latest minor: 3.2.0 → minor/patch PR
     }
 
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.80.0, <= 4.43.0"  # latest minor/patch: 4.43.0 → minor PR
+      version = ">= 3.80.0, <= 4.41.0"  # latest minor/patch: 4.43.0 → minor PR
     }
 
     google = {
       source  = "hashicorp/google"
-      version = "7.2.0"  # latest 5.x → major PR if allowed
+      version = "3.0.0"  # latest 5.x → major PR if allowed
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.38.0"  # latest 3.x → major PR
+      version = "2.0.0"  # latest 3.x → major PR
     }
 
     azurecaf = {
       source  = "aztfmod/azurecaf"
-      version = "~> 1.2.31"  # latest 1.2.x → minor PR
+      version = "~> 1.1.15"  # latest 1.2.x → minor PR
     }
 
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 3.0.2"  # latest 2.5.x → minor/patch PR
+      version = "~> 2.0.2"  # latest 2.5.x → minor/patch PR
     }
   }
 }
